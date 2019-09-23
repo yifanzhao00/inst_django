40eff82b095296e368e70e26175fb363352d8b48
zhao-yifan <yifan.zhao000@gmail.com>
2019-09-22 22:02:17 -0400

    implement and test a static page

--- inst/urls.py
+++ inst/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from inst.views import HelloWorld
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('inst/', include('inst.urls')),
+    path('', HelloWorld.as_view(), name='helloworld'),
 ]
